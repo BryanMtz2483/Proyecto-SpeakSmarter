7abf5bf52275b78dc3063f34e229c182c70efb52
BryanMtz2483 <mhzb3072@gmail.com>
2025-07-03 13:56:12 -0600

    Terminados de definir los permisos, roles y los respectivos permisos que tendrán cada uno de ellos, definimos las props globales de cada componente de VUE que nos servirá para mandar los roles y sus respectivos permisos a cada vista. Por último se modificó parte de la interfaz para testear si funcionó lo anterior

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -28,16 +28,20 @@ class HandleInertiaRequests extends Middleware
 
     /**
      * Define the props that are shared by default.
+     * los “props” (properties) son simplemente datos que se comparten desde el backend (Laravel) hacia el frontend (Vue, React, etc.) en cada página renderizada por Inertia.
      *
      * @see https://inertiajs.com/shared-data
      *
      * @return array<string, mixed>
      */
     public function share(Request $request): array
-    {
-        return [
-            ...parent::share($request),
-            //
-        ];
+{
+    return array_merge(parent::share($request), [
+        'user.roles' => $request->user() ? $request->user()->roles->pluck('name') : [],
+        'user.permissions' => $request->user() ? $request->user()->getPermissionsViaRoles()->pluck('name') : [],
+    ]); //array_merge() toma el array que retorna parent::share($request) y le añade tus props personalizados. Hazlo de esta manera siempre
     }
+    /*  El método SHARE se encuentra en tu App\Http\Middleware\HandleInertiaRequests y se encarga de:
+        1. Compartir props globales que estarán disponibles en TODAS las páginas de tu app frontend.
+        2. Se ejecuta en cada petición que devuelve una vista Inertia.*/
 }
